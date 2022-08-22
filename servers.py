7481a70069687a65e08b6c9b9329d786d292096e
unknown <alanrioscosta@yahoo.com>
2022-08-21 22:40:13 -0300

    novo padrão url zeppshered

--- servers.py
+++ servers.py
@@ -1,11 +1,12 @@
 import lxml.html
-from pytest import importorskip
 import requests
-import re 
+import re
+import math
 
 
 compilado = re.compile('("[\/A-z0-9\+(%)\-." ]+)')
 # (\([1-9 % +]+\))
+
 
 def zshared(link: str) -> str:
     response = requests.get(link)
@@ -13,7 +14,13 @@ def zshared(link: str) -> str:
     parser = lxml.html.fromstring(response.text)
 
     javascript = parser.xpath('//script[@type="text/javascript"]')[5].text
-    javascript = re.search(compilado, javascript).group(0).replace('+ (', '+ str(')
-    download_path = eval(javascript)
-    return URL + download_path
+    base = int(javascript.split()[3].replace(';', ''))
+    before_path = javascript.split()[-3].split('"')[1]
+    after_path = javascript.split()[-2].split('"')[1]
+    number_path = str(int(math.pow(base, 3) + 3))
 
+    return URL + before_path + number_path + after_path
+    
+
+#breakpoint()
+zshared('https://www38.zippyshare.com/v/zHYZqNnZ/file.html')
